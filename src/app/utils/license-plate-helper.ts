93e9f6577a4581f99535315a0ab818e28459746a
LyProjectCode <lucky_liyang@126.com>
2019-05-29 08:57:17 +0800

    update utils

--- src/app/utils/license-plate-helper.ts
+++ src/app/utils/license-plate-helper.ts
@@ -10,13 +10,6 @@ export enum PlateNumberState {
   other // 其他
 }
 
-export const EnergyType = {
-  firstD: 'D',
-  firstF: 'F',
-  lastD: 'D',
-  lastF: 'F'
-};
-
 export class LicensePlateHelper {
 
   public static PlateNumberType(plateNumber: string): number {
@@ -24,22 +17,19 @@ export class LicensePlateHelper {
       return PlateNumberState.invaild;
     }
     const verifyPlateNumber = plateNumber.slice(2); // 去掉第一位的汉字和第二位的字母
-    const regex1 = /^[A-HJ-NP-Z\d]{4}学|领|警$/;
-    const regex2 = /^粤[A-HJ-NP-Z\d][A-HJ-NP-Z\d]{4}港|澳$/;
-    const regex3 = /^\d{6}使$/;
-    const regex4 = /^[D|F][A-HJ-NP-Z\d]\d{4}$/;
-    const regex5 = /^\d{5}[D|F]$/;
-    if (regex1.test(verifyPlateNumber) || regex2.test(verifyPlateNumber) || regex3.test(verifyPlateNumber)) {// 判断是否为特殊车牌
+    const regex1 = /^[A-HJ-NP-Z\d]{4}学|领|警|港|澳$/;
+    const regex2 = /^\d{4}使$/;
+    const regex3 = /^[D|F][A-HJ-NP-Z\d]\d{4}$/;
+    const regex4 = /^\d{5}[D|F]$/;
+    if (regex1.test(verifyPlateNumber) || regex2.test(verifyPlateNumber)) {// 判断是否为特殊车牌
       return PlateNumberState.special;
     } else if (verifyPlateNumber.length === 5) { // 非特殊车牌，长度为5位的需要用户选择黄蓝号牌
       return PlateNumberState.yellowOrBlue;
     } else if (verifyPlateNumber.length === 6) { // 判断是否为小型新能源车或者大型新能源车
-      if (regex4.test(verifyPlateNumber)) {
+      if (regex3.test(verifyPlateNumber)) {
         return PlateNumberState.small_energy;
-      } else if (regex5.test(verifyPlateNumber)) {
+      } else if (regex4.test(verifyPlateNumber)) {
         return PlateNumberState.big_energy;
-      } else {
-        return PlateNumberState.invaild;
       }
     }
     return PlateNumberState.other;
